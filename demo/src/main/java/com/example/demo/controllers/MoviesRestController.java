de7d3b6a3e88fd08ad67465dba9faa64957476b5
Marius Marinescu <marinescumarius00@gmail.com>
2020-12-09 18:47:43 +0200

    added method to look up movies by Year

--- demo/src/main/java/com/example/demo/controllers/MoviesRestController.java
+++ demo/src/main/java/com/example/demo/controllers/MoviesRestController.java
@@ -3,11 +3,14 @@ package com.example.demo.controllers;
 import com.example.demo.models.Movies;
 import com.example.demo.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/movies")
@@ -20,5 +23,10 @@ public class MoviesRestController {
     @GetMapping()
     public List<Movies> getMoviesList(){
         return (List<Movies>) movieRepository.findAll();
+    }
+
+    @GetMapping("/byYear")
+    public Set<Movies> getMoviesByYear (@PathParam("minYear") int minYear, @PathParam("maxYear") int maxYear) {
+        return movieRepository.selectMoviesByYear(minYear, maxYear);
     }
 }
