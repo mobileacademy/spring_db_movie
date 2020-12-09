987dd7d3c7eb77f52620c6ae88f5243b7e64d806
Marius Marinescu <marinescumarius00@gmail.com>
2020-12-09 18:47:51 +0200

    added method to look up movies by Year

--- demo/src/main/java/com/example/demo/repositories/MovieRepository.java
+++ demo/src/main/java/com/example/demo/repositories/MovieRepository.java
@@ -1,9 +1,17 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.Movies;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movies, Long> {
+
+    @Query("select * from Movies where year between :min and :max")
+    public Set<Movies> selectMoviesByYear(@Param("year") int min, @Param("year")  int max) ;
+
 }
